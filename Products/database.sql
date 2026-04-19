193b45557d3c6fc78b7a23d783623d9a2f022746
MerjohnWeb <merjohnmacalisang@gmail.com>
2026-04-19 19:51:28 +0800

    Delete Products directory

--- Products/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE product_api;
-
-USE product_api;
-
-CREATE TABLE products (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    product VARCHAR(255) NOT NULL,
-    price DECIMAL(10,2) NOT NULL
-);
